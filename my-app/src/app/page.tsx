12386f6bf62475955dcced2626ac7085edfefaa5
Ammad Ali <AmmadAli@gmail.com>
2025-01-05 09:15:20 +0500

    first commit

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+import Header from "./components/header/page"
+import Footer from "./components/footer/page"
+import Main from "./components/main/page"
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
